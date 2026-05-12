8750e78d1f7173fd88731261c8ead877e06e33d3
rtjhussein <rtjhussein@gmail.com>
2026-05-12 23:56:55 +0200

    Build entry page slice

--- shona_api/web/views.py
+++ shona_api/web/views.py
@@ -8,4 +8,25 @@ class DictionarySearchView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["search_endpoint"] = reverse("search")
+        context["entry_url_template"] = reverse(
+            "dictionary-entry",
+            kwargs={"public_id": "__PUBLIC_ID__"},
+        )
+        return context
+
+
+class DictionaryEntryView(TemplateView):
+    template_name = "web/dictionary_entry.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        public_id = self.kwargs["public_id"]
+        context["lemma_public_id"] = public_id
+        context["lemma_endpoint"] = reverse(
+            "lemma-read",
+            kwargs={"public_id": public_id},
+        )
+        context["search_url"] = reverse("dictionary-search")
+        context["tsumo_endpoint"] = reverse("tsumo-list")
+        context["madimikira_endpoint"] = reverse("madimikira-list")
         return context
